dcc5bad9a9443fa87203e7b55c1546bb9acecde8
MdTopu6092 <mdtowhidulislam9212@gmail.com>
2022-04-04 08:07:16 +0600

    first commit

---
+++ milstone-1/math-operation.js
@@ -0,0 +1,10 @@
+var price1 = 92;
+var price2 = 60;
+var sum = price1 + price2;
+var difference = price1 - price2;
+var multification = price1 * price2;
+var division = price1/ price2;
+console.log(sum);
+console.log(difference);
+console.log(multification);
+console.log(division);
